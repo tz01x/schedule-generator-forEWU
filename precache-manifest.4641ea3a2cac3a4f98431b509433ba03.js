ac65cc88dd9d9eaf3b7deb66ccb02fcbf795a16f
tumzied <abdur963rahman@gmil.com>
2020-06-23 13:54:41 +0600

    Updates

---
+++ precache-manifest.4641ea3a2cac3a4f98431b509433ba03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf64de719b180c5cadea5badc62b1588",
+    "url": "/schedule-generator-forEWU/index.html"
+  },
+  {
+    "revision": "92e6cecb584512f925c5",
+    "url": "/schedule-generator-forEWU/static/css/main.cd9569f8.chunk.css"
+  },
+  {
+    "revision": "9de960ce944e8fe5cfe5",
+    "url": "/schedule-generator-forEWU/static/js/2.2d6e78fe.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/schedule-generator-forEWU/static/js/2.2d6e78fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92e6cecb584512f925c5",
+    "url": "/schedule-generator-forEWU/static/js/main.6d61f142.chunk.js"
+  },
+  {
+    "revision": "63a50c268c9a49dc6c7c",
+    "url": "/schedule-generator-forEWU/static/js/runtime-main.e500950d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/schedule-generator-forEWU/static/media/logo.5d5d9eef.svg"
+  }
+]);
