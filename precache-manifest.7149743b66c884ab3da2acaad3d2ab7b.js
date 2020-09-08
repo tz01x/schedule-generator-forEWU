fb23356970170ae4727ff53546097f3610188d9c
tumzied <abdur963rahman@gmil.com>
2020-09-08 16:57:12 +0600

    Updates

--- precache-manifest.7149743b66c884ab3da2acaad3d2ab7b.js
+++ precache-manifest.7149743b66c884ab3da2acaad3d2ab7b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2a5b910e4354d5fe8e2df7236fa73da0",
+    "revision": "8d0b82d805afc92ee4bc582f02d075b7",
     "url": "/schedule-generator-forEWU/index.html"
   },
   {
-    "revision": "91d137c87ce61ebc62f4",
+    "revision": "d2fbd94204c3735c562c",
     "url": "/schedule-generator-forEWU/static/css/main.440a2cd0.chunk.css"
   },
   {
-    "revision": "39b460030a70bcb609eb",
-    "url": "/schedule-generator-forEWU/static/js/2.44447b88.chunk.js"
+    "revision": "4c41cf087330c6267b71",
+    "url": "/schedule-generator-forEWU/static/js/2.69efff4c.chunk.js"
   },
   {
-    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
-    "url": "/schedule-generator-forEWU/static/js/2.44447b88.chunk.js.LICENSE.txt"
+    "revision": "c18988918784917b47f83d176b87cdc7",
+    "url": "/schedule-generator-forEWU/static/js/2.69efff4c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "91d137c87ce61ebc62f4",
-    "url": "/schedule-generator-forEWU/static/js/main.478e907a.chunk.js"
+    "revision": "d2fbd94204c3735c562c",
+    "url": "/schedule-generator-forEWU/static/js/main.ff94fc90.chunk.js"
   },
   {
     "revision": "63a50c268c9a49dc6c7c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/schedule-generator-forEWU/precache-manifest.875bbab2787b6a25f45aeb2481e51c3a.js"
+  "/schedule-generator-forEWU/precache-manifest.7149743b66c884ab3da2acaad3d2ab7b.js"
 );
 
 self.addEventListener('message', (event) => {
